9a5a60bc7a84e1798992295055b10b5c1fd342a6
Michael Vu <email@michael.vu>
2022-11-24 14:48:37 +0000

    init

---
+++ Dockerfiles/installer.sh
@@ -0,0 +1,8 @@
+mkdir /tmp/setup
+cd /tmp/setup
+wget https://projects.unbit.it/downloads/uwsgi-latest.tar.gz -cO _
+tar zxf _
+cd uwsgi*
+python3 uwsgiconfig.py --build psgi
+mv uwsgi /usr/local/bin/uwsgi
+cd /tmp; rm -fr setup
