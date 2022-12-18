1cc5f7065501c58a2cfc20c38b3fbe369e02aa32
Michael Vu <email@michael.vu>
2022-12-18 20:49:26 +0800

    Separate uwsgi plugins build and storage for later recall
    The changes to the installer.sh script and the addition of the uwsgi.yml file
    in the docker-compose directory are intended to separate the build of the psgi
    and http uwsgi plugins and store them in a location that can be accessed later
    when running uwsgi. Previously, the plugins were being built and stored in the
    /tmp directory, which would not be persisted after the container was stopped.
    Storing the plugins in the /usr/local/bin directory allows them to be accessed
    later when needed. The change to the web.conf file is intended to specify the
    directory containing the plugins so that uwsgi can find them when it is run.

--- Dockerfiles/installer.sh
+++ Dockerfiles/installer.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 mkdir /tmp/setup
+
 cd /tmp/setup
 wget https://projects.unbit.it/downloads/uwsgi-latest.tar.gz -cO _
 tar zxf _
+
 cd uwsgi*
+
+python3 uwsgiconfig.py --plugin plugins/psgi core
+find /tmp -name psgi_plugin.so -exec mv {} /usr/local/bin \;
+
+python3 uwsgiconfig.py --plugin plugins/http core
+find /tmp -name http_plugin.so -exec mv {} /usr/local/bin \;
+
 python3 uwsgiconfig.py --build psgi
 mv uwsgi /usr/local/bin/uwsgi
+
 cd /tmp; rm -fr setup
